cb63d4b193ba2e7044ba75834b0c88c6a614b349
Erzhena <edulzynova@alfabank.ru>
2018-09-23 17:55:13 +0300

    added dataBinding

--- app/src/main/java/com/example/erzhena/starwars/adapters/StarWarsFilmsAdapter.java
+++ app/src/main/java/com/example/erzhena/starwars/adapters/StarWarsFilmsAdapter.java
@@ -1,39 +1,40 @@
 package com.example.erzhena.starwars.adapters;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.erzhena.starwars.R;
+import com.example.erzhena.starwars.databinding.FilmItemBinding;
 import com.example.erzhena.starwars.models.FilmsResults;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class StarWarsFilmsAdapter extends RecyclerView.Adapter<StarWarsFilmsAdapter.ViewHolder> {
 
     private List<FilmsResults> filmsResults;
-    private LayoutInflater mInflater;
+    private LayoutInflater inflater;
     private ItemClickListener mClickListener;
 
     public StarWarsFilmsAdapter(Context context, List<FilmsResults> data) {
-        this.mInflater = LayoutInflater.from(context);
+        this.inflater = LayoutInflater.from(context);
         this.filmsResults = data;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.film_item, parent, false);
-        return new ViewHolder(view);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        FilmItemBinding binding = DataBindingUtil.inflate(inflater, R.layout.film_item, parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        FilmsResults results = filmsResults.get(position);
-        holder.newsTitle.setText(results.toString());
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(filmsResults.get(position));
     }
 
     @Override
@@ -43,22 +44,22 @@ public class StarWarsFilmsAdapter extends RecyclerView.Adapter<StarWarsFilmsAdap
 
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView newsTitle;
+        FilmItemBinding binding;
 
-        ViewHolder(View itemView) {
-            super(itemView);
-            newsTitle = itemView.findViewById(R.id.newsTitle);
-            itemView.setOnClickListener(this);
+        public ViewHolder(FilmItemBinding filmItemBinding) {
+            super(filmItemBinding.getRoot());
+            this.binding = filmItemBinding;
+        }
+
+        public void bind(FilmsResults filmsResults) {
+            binding.setFilmResults(filmsResults);
+            binding.executePendingBindings();
         }
 
         @Override
         public void onClick(View view) {
             if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
         }
-    }
-
-    FilmsResults getItem(int id) {
-        return filmsResults.get(id);
     }
 
     void setClickListener(ItemClickListener itemClickListener) {
